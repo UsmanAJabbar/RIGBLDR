4a1d894f01144e14b847ae8ba59f89c06e2811f3
Tony Sims <zocle@zocle.net>
2021-09-02 20:49:28 +0000

    finished video_cards table with default data and fill script

--- backend/controllers/video_cards.js
+++ backend/controllers/video_cards.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-const { VideoCards, Apis } = db.models;
+const { VideoCards, VendorEndpoints } = db.models;
 const Op = db.Sequelize.Op;
 
 
@@ -10,7 +10,7 @@ exports.create = (req, res) => {
 exports.findOne = (req, res) => {
   const videoCard = VideoCards.findOne({
     where: { id: req.params.id },
-    include: [ { model: Apis, attributes: ['price', 'in_stock'] } ] })
+    include: [ { model: VendorEndpoints, attributes: ['price', 'in_stock'] } ] })
 	  .then(data => {
 		  res.send(data);
 		})
@@ -23,14 +23,19 @@ exports.findOne = (req, res) => {
 };
 
 exports.findAll = (req, res) => {
-  const { core, socket, manufacturer } = req.query;
+  const { model, manufacturer, chipset_manufacturer, chipset, memory, core_clock, boost_clock, pcie_gen } = req.query;
   const filters = {};
-  (name) ? filters.name = {[Op.like]: `${name}%`} : null;
-  (chipset) ? filters.chipset = {[Op.like]: `${chipset}%`} : null;
+  (model) ? filters.model = {[Op.like]: `${model}%`} : null;
   (manufacturer) ? filters.manufacturer = {[Op.like]: `${manufacturer}%`} : null;
+  (chipset_manufacturer) ? filters.chipset_manufacturer = {[Op.like]: `${chipset_manufacturer}%`} : null;
+  (chipset) ? filters.chipset = {[Op.like]: `${chipset}%`} : null;
+  (memory) ? filters.memory = {[Op.like]: `${memory}%`} : null;
+  (core_clock) ? filters.core_clock = {[Op.like]: `${core_clock}%`} : null;
+  (boost_clock) ? filters.boost_clock = {[Op.like]: `${boost_clock}%`} : null;
+  (pcie_gen) ? filters.pcie_gen = {[Op.like]: `${pcie_gen}%`} : null;
   const videoCard = VideoCards.findAll({
 		where: filters,
-    include: [ { model: Apis, attributes: ['price', 'in_stock'] } ] })
+    include: [ { model: VendorEndpoints, attributes: ['price', 'in_stock'] } ] })
 	  .then(data => {
 		  res.send(data);
 		})
