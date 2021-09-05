9d6b94d07eec381dc897fbee9a5b4907cc66697b
Tony Sims <zocle@zocle.net>
2021-09-05 19:24:43 +0000

    added cors to processor and video_cards routes

--- backend/routes/video_cards.js
+++ backend/routes/video_cards.js
@@ -2,6 +2,8 @@ module.exports = app => {
 	const videoCards = require("../controllers/video_cards.js");
 
 	const router = require("express").Router();
+	const cors = require("cors");
+  router.use(cors());
 
 	router.get("/", videoCards.findAll);
 	router.get("/:id", videoCards.findOne);
