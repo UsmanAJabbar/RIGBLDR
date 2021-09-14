7e3681fde2f3b798b1127fd6b039878bf51ebe83
UsmanGTA <hi@usmanjabbar.com>
2021-09-14 12:07:17 +0500

    Added Proceed Button

--- src/Build/Build.js
+++ src/Build/Build.js
@@ -9,25 +9,40 @@ import './Build.css';
 
 class Build extends React.Component {
   render() {
+
     return (
       <AppContext.Consumer>
       { 
         appJsState => (
           <ContentBox>
             <div className="build-overview" style={{ display: 'flex', fontWeight: '800 !' }}>
-              <h1>PC</h1>
-              <div className="build-overview-container">
-                <h4>Estimated Power Usage</h4>
+              <h1 style={{width: '50%'}}>PC</h1>
+              <div style={{width: '25%'}} className="build-overview-container">
+                <h4>Power</h4>
                 <h4>
                   {
                     // TDP Calculator
+                    // console.log(appJsState)
                     `${Object.values(appJsState.selected)
-                        .reduce((accumulatedValue, currentPartRow) => accumulatedValue + parseInt(currentPartRow.tdp), 0)
+                        .reduce(
+                          (total, currentPart) => total + (currentPart.tdp || 0), 0)
                         || 0
                       }W`
                   }
                 </h4>
               </div>
+              <div style={{width: '25%'}} className="build-overview-container">
+                <h4>Total</h4>
+                  <h4>
+                    {
+                      `$${Object.values(appJsState.selected)
+                          .reduce(
+                            (total, currentPart) => total + (Math.round((currentPart?.vendor_endpoint?.price || 0) * 100) / 100),
+                          0) || 0
+                        }`
+                    }
+                  </h4>
+                </div>
             </div>
             <table id="build" style={{ width: '100%' }}>
               <thead>
@@ -50,9 +65,21 @@ class Build extends React.Component {
                 }
               </tbody>
             </table>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', marginTop: '1rem' }}>
-              <Button overrides={{ text: { textTransform: 'uppercase', fontWeight: 700 }}}>Proceed</Button>
-            </div>
+            <a target="_blank" rel="noreferrer" href={
+              `https://www.amazon.com/gp/aws/cart/add.html${
+                Object.values(appJsState.selected)
+                  .reduce((link, currentPart, index) =>
+                    (currentPart?.vendor_endpoint?.vendor_product_id)
+                      ? `${link + ((index === 0) ? '?': '&')}` + `ASIN.${index + 1}=${currentPart?.vendor_endpoint?.vendor_product_id}&Quantity.${index + 1}=1`
+                      : link
+                    , ''
+                  )
+              }`
+            }>
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', marginTop: '1rem' }}>
+                <Button overrides={{ text: { textTransform: 'uppercase', fontWeight: 700 }}}>Proceed</Button>
+              </div>
+            </a>
           </ContentBox>
         )
       }
