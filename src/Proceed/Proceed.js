d8debb907b70219193d60f703eb5ba4209abe0d4
Tony Sims <zocle@zocle.net>
2021-09-14 01:32:48 +0000

    simple redirect using window.location

--- src/Proceed/Proceed.js
+++ src/Proceed/Proceed.js
@@ -1,19 +1,7 @@
-import React from 'react';
-import { Redirect } from "react-router-dom";
+import React, { useEffect } from 'react';
 
 class Proceed extends React.Component {
-  render() {
-    return (
-	    window.location.href="http://google.com"
-     // <Redirect push
-      //  to={{
-//		pathname: "http://google.com",
- //         search: "?utm=your+face",
-    //      state: { referrer: currentLocation }
-  //      }}
-   //   />
-    )
-  }
+  render() { return window.location.href="http://amazon.com" }
 }
 
 export default Proceed;
